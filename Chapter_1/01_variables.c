52a3397e0c46083e320ee7c176e36b555b402c5d
Guddu Kumar <guddu12022003@gmail.com>
2025-05-15 23:27:51 +0530

    Add files via upload

---
+++ Chapter_1/01_variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  int a = 5; // Declare an integer variable 'a' and initialize it to 5
+  a = 10; // Change the value of 'a' to 10
+  
+  printf("Value of a : %d/n", a);
+  return 0;
+}
