e9c7aa0e28c9db76a576da10df9348a1851457a1
Guddu Kumar <guddu12022003@gmail.com>
2025-05-16 23:38:28 +0530

    Update 01_variables.c

--- Chapter_1/01_variables.c
+++ Chapter_1/01_variables.c
@@ -3,7 +3,6 @@ int main()
 {
   int a = 5; // Declare an integer variable 'a' and initialize it to 5
   a = 10; // Change the value of 'a' to 10
-  
   printf("Value of a : %d/n", a);
   return 0;
 }
