92a6c4f7172498da7f17391391e23fa39d8b51cd
Guddu Kumar <guddu12022003@gmail.com>
2025-05-19 23:59:35 +0530

    Update 03_data_types.c

--- Chapter_1/03_data_types.c
+++ Chapter_1/03_data_types.c
@@ -12,6 +12,7 @@ int main(){
   // char c;
   // c = 'a';
   char c = 'a';
+  
   printf("The value of a is: %d\n", a);
   printf("The value of b is: %f\n", b);
   printf("The value of c is: %c\n", c);
